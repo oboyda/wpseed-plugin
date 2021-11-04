33548f2c41fbbd91cd93307c6231299061b1d653
oleksiyboyda <oleksiyboyda@oleksiys-mbp.home>
2021-11-04 12:21:15 +0100

    Update 1.4.0

--- plugin.php
+++ plugin.php
@@ -2,28 +2,31 @@
 /**
  * Plugin Name: WPPB Plugin
  * Description: Bootstrap plugin that makes it easier to start a new plugin development
- * Version: 1.3.dev
+ * Version: 1.4.0
  * Author: Alexis Boyda
  * Author URI: https://aleapp.com
  * Text Domain: wppb
  */
 
 define('WPPB_NAME', 'WPPB Plugin');
-define('WPPB_VERSION', '1.3.dev');
+define('WPPB_VERSION', '1.4.0');
 define('WPPB_DIR', dirname(__FILE__));
 define('WPPB_INDEX', plugins_url('', __FILE__));
 
 add_action('plugins_loaded', function()
 {
     require WPPB_DIR . '/src/setup.php';
-    require WPPB_DIR . '/src/functions.php';
+    require WPPB_DIR . '/vendor/autoload.php';
     
-    if(wppb_check_deps([
-        //'woocommerce/woocommerce.php'
-    ]))
+    $deps = new \WPPB\Deps([
+//        'woocommerce/woocommerce.php'
+    ]);
+    
+    if($deps->check())
     {
-        require WPPB_DIR . '/vendor/autoload.php';
         require WPPB_DIR . '/src/classes/load.php';
+        require WPPB_DIR . '/src/functions.php';
+        require WPPB_DIR . '/src/debug.php';
         require WPPB_DIR . '/src/scripts.php';
     }
     
