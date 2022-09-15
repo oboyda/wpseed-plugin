b13f3998da5fcdc1062a6219976750a184aab967
aleapp <admin@aleapp.com>
2022-09-15 10:04:56 +0000

    updates

--- src/php/setup.php
+++ src/php/setup.php
@@ -8,5 +8,5 @@ add_action('init', 'pboot_load_textdomain');
 
 function pboot_load_textdomain()
 {
-    load_plugin_textdomain('pboot', false, PBOOT_DIR . '/langs');
+    load_plugin_textdomain('pboot', false, plugin_basename(PBOOT_DIR) . '/langs');
 }
