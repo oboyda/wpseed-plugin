d6342372fafd59237daa56c5d57a8a978ff8a3b3
aleapp <admin@aleapp.com>
2022-11-21 11:09:26 +0000

    updates

--- src/php/utils.php
+++ src/php/utils.php
@@ -19,11 +19,3 @@ function pboot_print_mod_view($mod, $view_name, $args=[])
 {
     pboot_get_mod_view($mod, $view_name, $args, true);
 }
-
-$pboot_settings = null;
-function pboot_get_option($name, $default=null)
-{
-    global $pboot_settings;
-    $opt = isset($pboot_settings) ? $pboot_settings->get_option($name) : false;
-    return (empty($opt) && isset($default)) ? $default : $opt;
-}
