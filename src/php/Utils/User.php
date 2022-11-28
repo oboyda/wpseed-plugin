3c3a1b4e06ff2e10435db1a01cfe01baa576ef76
aleapp <admin@aleapp.com>
2022-11-28 14:39:44 +0000

    updates

--- src/php/Utils/User.php
+++ src/php/Utils/User.php
@@ -6,11 +6,11 @@ class Base extends \WPSEEDE\Utils\Base
 {
     static function getCurrentUser($type_class='\PBOOT\Type\User')
     {
-        parent::getCurrentUser($type_class);
+        return parent::getCurrentUser($type_class);
     }
 
     static function getCurrentUserRole($type_class='\PBOOT\Type\User')
     {
-        parent::getCurrentUserRole($type_class);
+        return parent::getCurrentUserRole($type_class);
     }
 }
