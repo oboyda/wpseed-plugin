c830e34cbad56208211acca504b0b0ae1a0cf9d4
aleapp <admin@aleapp.com>
2022-12-03 13:38:51 +0000

    update

--- src/php/Type/Post.php
+++ src/php/Type/Post.php
@@ -2,153 +2,17 @@
 
 namespace PBOOT\Type;
 
-use PBOOT\Utils\Base as Utils_Base;
-
 class Post extends \WPSEED\Post
 {
     public function __construct($post=null)
     {
-        parent::__construct($post, static::_get_props_config());
+        parent::__construct($post, self::_get_props_config());
     }
 
-    public function getPropConfigData($key, $data_key=null, $default=null)
+    static function _get_props_config()
     {
-        $prop_config = $this->get_props_config($key);
-
-        if(isset($data_key))
-        {
-            return isset($prop_config[$data_key]) ? $prop_config[$data_key] : $default;
-        }
-
-        return isset($prop_config) ? $prop_config : $default;
-    }
-
-    public function getPropOptionLabel($key, $option_value)
-    {
-        $options = $this->getPropConfigData($key, 'options', []);
-
-        return isset($options[$option_value]) ? $options[$option_value] : false;
-    }
-
-    public function getId()
-    {
-        return $this->get_id();
-    }
-
-    public function getTitle()
-    {
-        return $this->get_data('post_title', '');
-    }
-
-    public function getExcerpt($autop=false)
-    {
-        $data = $this->get_data('post_excerpt', '');
-
-        if($data)
-        {
-            if($autop)
-            {
-                $data = wpautop($data);
-            }
-        }
-
-        return $data;
-    }
-
-    public function getContent($autop=false, $words_num=null)
-    {
-        $data = $this->get_data('post_content', '');
-
-        if($data)
-        {
-            if(isset($words_num))
-            {
-                $data = wp_trim_words($data, $words_num);
-            }
-            if($autop)
-            {
-                $data = wpautop($data);
-            }
-        }
-
-        return $data;
-    }
-
-    public function getLink()
-    {
-        return $this->get_permalink();
-    }
-
-    public function getAuthor()
-    {
-        return $this->get_data('post_author');
-    }
-
-    /*
-    Images
-    -------------------------
-    */
-
-    public function getFeaturedImageId()
-    {
-        return get_post_thumbnail_id($this->get_id());
-    }
-    public function getFeaturedImageSrc($size='thumbnail', $placeholder=false)
-    {
-        $image_id = $this->getFeaturedImageId();
-        $att_image_src = $image_id ? wp_get_attachment_image_src($image_id, $size) : [];
-        $image_src = ($att_image_src && isset($att_image_src[0])) ? $att_image_src[0] : '';
-
-        return (empty($image_src) && $placeholder) ? (function_exists('wc_placeholder_img_src') ? wc_placeholder_img_src($size) : $image_src) : $image_src;
-    }
-    public function hasFeaturedImage()
-    {
-        $image_id = $this->getFeaturedImageId();
-        return !empty($image_id);
-    }
-
-    /*
-    Helpers
-    -------------------------
-    */
-
-    public function hasProp($key)
-    {
-        return $this->has_prop($key);
-    }
-
-    public function getProp($key, $default=null, $single=true)
-    {
-        return $this->get_prop($key, $default, $single);
-    }
-
-    public function getTerms($taxonomy, $fields='ids', $single=true)
-    {
-        $term_ids = $this->get_prop($taxonomy);
-
-        if($fields === 'ids')
-        {
-            return $single ? (isset($term_ids[0]) ? $term_ids[0] : false) : $term_ids;
-        }
-
-        $args = [
-            'taxonomy' => $taxonomy,
-            'include' => $term_ids,
-            'fields' => $fields
-        ];
-
-        $terms = get_terms($args);
-
-        if(is_wp_error($terms))
-        {
-            return $single ? false : [];
-        }
-
-        if($single)
-        {
-            return isset($terms[0]) ? $terms[0] : false;
-        }
-
-        return $terms;
+        return array_merge(parent::_get_props_config(), [
+            
+        ]);
     }
 }
--- src/php/Type/User.php
+++ src/php/Type/User.php
@@ -2,66 +2,17 @@
 
 namespace PBOOT\Type;
 
-class User extends \WPSEED\User
+class User extends \WPSEEDE\User 
 {
     public function __construct($user=null)
     {
-        parent::__construct($user);
+        parent::__construct($user, self::_get_props_config());
     }
 
     static function _get_props_config()
     {
-        return [
-            'email_verified' => [
-                'type' => 'meta'
-            ]
-        ];
-    }
-
-    /* ------------------------- */
-
-    public function getId()
-    {
-        return $this->get_id();
-    }
-
-    public function getRole()
-    {
-        return $this->get_role();
-    }
-
-    public function getLogin()
-    {
-        return $this->get_data('user_login', '');
-    }
-
-    public function getEmail()
-    {
-        return $this->get_data('user_email', '');
-    }
-
-    public function getFirstName()
-    {
-        return $this->get_meta('first_name', true, '');
-    }
-
-    public function getLastName()
-    {
-        return $this->get_meta('last_name', true, '');
-    }
-
-    public function getFullName()
-    {
-        return trim($this->getFirstName() . ' ' . $this->getLastName());
-    }
-
-    public function isEmailVerified()
-    {
-        if(in_array($this->getRole(), ['administrator']))
-        {
-            return true;
-        }
-
-        return (bool)$this->getMeta('email_verified', true);
+        return array_merge(parent::_get_props_config(), [
+            
+        ]);
     }
 }
