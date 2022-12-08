3382f0664308d8cbb3d1de52c79604d2c79f4a88
aleapp <admin@aleapp.com>
2022-12-08 08:29:13 +0000

    updates

--- config/acf-blocks.php
+++ config/acf-blocks.php
@@ -9,6 +9,8 @@ add_action('acf/init', 'pboot_register_blocks');
 
 function pboot_register_blocks()
 {
+    global $pboot_setup;
+    
     if(function_exists('acf_register_block'))
     {
         /*
@@ -19,7 +21,7 @@ function pboot_register_blocks()
             'name'				=> 'test-block',
             'title'				=> __('PBOOT Test Block', 'pboot'),
             //'description'		=> __('Block description.', 'pboot'),
-            'render_callback'	=> 'pboot_render_block_view',
+            'render_callback'	=> [$pboot_setup->view_loader, 'renderViewAcf'],
             'category'			=> 'pboot-blocks',
             //'icon'				=> 'admin-comments'
             //'keywords'			=> [''],
@@ -27,24 +29,4 @@ function pboot_register_blocks()
             'mode' => 'auto'
         ]);
     }
-}
-
-/*
-* Register callback wrapper function
-* ----------------------------------------
-*/
-function pboot_render_block_view($block, $content, $is_preview, $post_id, $wp_block, $context)
-{
-    $acf_prefix = 'acf/';
-
-    $view = (strpos($block['name'], $acf_prefix) === 0) ? substr($block['name'], strlen($acf_prefix)) : $block['name'];
-    $view_args = isset($block['view_args']) ? $block['view_args'] : [];
-
-    // $view_args['block_id'] = isset($block['id']) ? $block['id'] : '';
-    $view_args['block_id'] = \WPSEEDE\Utils\Base::getBlockId($wp_block);
-    $view_args['data'] = isset($block['data']) ? $block['data'] : '';
-
-    $view_args['html_class'] = isset($block['className']) ? $block['className'] : '';
-
-    echo pboot_get_view($view, $view_args);
 }
