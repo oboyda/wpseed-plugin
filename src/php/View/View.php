ee87455a54d79127b5cd8d87bcc28bc8ee012950
aleapp <admin@aleapp.com>
2023-01-09 17:24:50 +0000

    updates

--- src/php/View/View.php
+++ src/php/View/View.php
@@ -4,12 +4,14 @@ namespace PBOOT\View;
 
 class View extends \WPSEEDE\View 
 {
-    const CONTEXT_NAME = 'pboot';
+    // const CONTEXT_NAME = 'pboot';
 
     public function __construct($args, $default_args=[])
     {
+        global $pboot_setup;
         parent::__construct($args, wp_parse_args($default_args, [
-
+            'view_loader' => $pboot_setup->view_loader,
+            'context_name' => $pboot_setup->context_name
         ]));
     }
 }
