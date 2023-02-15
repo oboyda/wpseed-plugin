0cff7f2ff7735378326664521315c257d2a0c19d
aleapp <admin@aleapp.com>
2023-02-15 11:24:01 +0000

    updates

--- src/php/utils.php
+++ src/php/utils.php
@@ -17,3 +17,17 @@ function pboot_print_view($view_name, $args=[])
     global $pboot_setup;
     $pboot_setup->view_loader->printView($view_name, $args);
 }
+
+function pboot_debug($debug, $file_name='__debug.txt', $append=false)
+{
+    $debug_path = ABSPATH . $file_name;
+    $debug_html = is_array($debug) ? print_r($debug, true) : $debug;
+
+    if($append)
+    {
+        file_put_contents($debug_path, $debug_html, FILE_APPEND);
+    }
+    else{
+        file_put_contents($debug_path, $debug_html);
+    }
+}
